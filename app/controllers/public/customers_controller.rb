0f6849f4ec64048516c9233201a3d54aee05548a
katakurashota <j230347g9327@gmail.com>
2022-03-07 13:13:12 +0000

    finish

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,4 +2,8 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(params[:id])
   end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end 
 end
